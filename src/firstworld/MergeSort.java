848f4247c35e5676b4f0d904cf9593800da295ca
neoDerby <neo.derby@yahoo.com>
2013-05-28 23:35:59 -0700

    Added mergesort

--- src/firstworld/MergeSort.java
+++ src/firstworld/MergeSort.java
@@ -2,9 +2,21 @@ package firstworld;
 
 import java.util.ArrayList;
 
-public class MergeSort {
-	
-	public  ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){		
+public class MergeSort {	
+	public  ArrayList<Integer> mergesort(ArrayList<Integer> a){
+		if (a.size() == 1)
+			return a; // it is already sorted		
+	    int mid = (a.size()/2);
+	    ArrayList<Integer>  lefthalf = new ArrayList<Integer>();
+	    ArrayList<Integer>  righthalf = new ArrayList<Integer>();
+	    
+	    for (int i = 0 ; i < mid ; i ++)
+	    	lefthalf.add(a.remove(0));
+	    while (!a.isEmpty())
+	    	righthalf.add(a.remove(0));		    
+		return merge( mergesort(lefthalf), mergesort( righthalf));
+	}
+	private  ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){		
 		if(a.isEmpty())
 			return b;
 		if(b.isEmpty())
@@ -13,12 +25,11 @@ public class MergeSort {
 			return concat (a.remove(0), merge(a,b));
 		else
 			return concat (b.remove(0), merge(a,b));
-	}
-	private ArrayList<Integer> concat(int elem, ArrayList<Integer> list)
-	{
+	}		
+	private ArrayList<Integer> concat(int elem, ArrayList<Integer> list){
 		ArrayList<Integer>  sortedlist = new ArrayList<Integer>();
 		sortedlist.add(elem);
 		sortedlist.addAll(list)	;
 		return sortedlist;
-	}	
+	}
 }
--- src/firstworld/SayHello.java
+++ src/firstworld/SayHello.java
@@ -16,10 +16,23 @@ public class SayHello {
 		//int a [] = new int[] {3,4,5,6};
 		//System.out.println("Sum of list:" + Arrays.toString(a) + " is " + ArraySum.arraySumAux(a, 0, 4));
 			
-		ArrayList<Integer> arl1 = new ArrayList<Integer>(Arrays.asList(0, 3,4, 5, 9));
-		ArrayList<Integer> arl2 = new ArrayList<Integer>(Arrays.asList(1, 2 ,6, 7, 8));
+		//ArrayList<Integer> arl1 = new ArrayList<Integer>(Arrays.asList(0, 3,4, 5, 9));
+		//ArrayList<Integer> arl2 = new ArrayList<Integer>(Arrays.asList(1, 2 ,6, 7, 8));
+		ArrayList<Integer> unsorted = new ArrayList<Integer>(Arrays.asList(10,18, 5, 1));
+		System.out.println( "The unsorted list is :" +  unsorted.toString());
+		/*
+		System.out.println( "The unsorted sorted list is :" +  unsorted.toString());
+		int mid = (unsorted.size()/2);
+		for (int i = 0 ; i < mid ; i++)
+			System.out.println(unsorted.remove(0));
 		
+		System.out.println( "The unsorted sorted list half size :" +  unsorted.size());
+		
+		while (!unsorted.isEmpty())
+			System.out.println(unsorted.remove(0));
+		*/
 		MergeSort ms = new MergeSort();
-		System.out.println( "The sorted list is :" +  ms.merge(arl1, arl2).toString());	
+		//System.out.println( "The sorted list is :" +  ms.merge(arl1, arl2).toString());
+		System.out.println( "The sorted list is :" +  ms.mergesort(unsorted).toString());
 	}		
 }
